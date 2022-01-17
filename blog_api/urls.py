c71430653c46cb762a0d1773513ae390e74334f4
Ankush <ankush357159@gmail.com>
2022-01-17 17:44:13 +0530

    user integration

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from blog_api.views import function_views
 from blog_api.views.class_based_views import PostDetails, PostLists
-from blog_api.views.generic_views  import PostDetail, CategoryDetail, PostListView, CategoryListView
+from blog_api.views.generic_views  import MyTokenObtainPairView, PostDetail, CategoryDetail, PostListView, CategoryListView, UserCreateView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     # Function based views
@@ -17,5 +18,10 @@ urlpatterns = [
     path('category/<str:pk>/', CategoryDetail.as_view()),
     path('post/', PostListView.as_view()),
     path('post/<int:pk>/',PostDetail.as_view()),
+
+    # User urls
+    path("login/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="logout"),
+    path('register_user/', UserCreateView.as_view())
 
 ]
--- blog_api/views/generic_views.py
+++ blog_api/views/generic_views.py
@@ -1,6 +1,15 @@
 from rest_framework import generics
 from blog_api.models import Category, Post
-from blog_api.serializers import CategorySerializer, PostSerializer
+from blog_api.serializers import CategorySerializer, MyTokenObtainPairSerializer, PostSerializer, RegisterUserSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+# JWT simple token view
+class MyTokenObtainPairView(TokenObtainPairView):
+    permission_classes = [AllowAny,]
+    serializer_class = MyTokenObtainPairSerializer   
 
 class PostListView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -17,3 +26,9 @@ class CategoryListView(generics.ListCreateAPIView):
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterUserSerializer
+    permission_classes = [IsAuthenticated,]
